68b5de75f49e1afee298719367950524f83449e9
Zach Lankton <zachlankton@gmail.com>
2021-09-17 02:14:10 -0400

    Updated Install

--- install.sh
+++ install.sh
@@ -3,18 +3,18 @@ echo "STARTING DOCKER LAMP STACK..."
 sleep 5
 
 echo "INSTALLING SYMFONY 3.4 WEBSITE SKELETON"
-docker exec -i task_manager composer create-project symfony/website-skeleton:"^3.4" app
+docker exec -i zachL_task_manager composer create-project symfony/website-skeleton:"^3.4" app
 
 echo "CHMODDING 777 so we have access to editing these files"
-docker exec -i -w /app task_manager chmod -R 777 .
+docker exec -i -w /app zachL_task_manager chmod -R 777 .
 
 echo "COPYING APPLICATION FILES"
 cp -rT ./app_files ./app
 echo "DONE."
 
 echo "CREATING DB"
-docker exec -i -w /app task_manager ./bin/console doctrine:database:create
-docker exec -i -w /app task_manager ./bin/console doctrine:migrations:migrate
+docker exec -i -w /app zachL_task_manager ./bin/console doctrine:database:create
+docker exec -i -w /app zachL_task_manager ./bin/console doctrine:migrations:migrate
 echo "DONE!"
 
 echo "Point your browser to http://localhost to see the Task Manager Demo!"
